4cdf26297f3df4ee29dd494c41c08db06d4afa45
hvtienprotv84 <dmx031117@gmail.om>
2024-07-21 06:01:30 +0700

    push

---
+++ README.md
@@ -0,0 +1,8 @@
+# Counter App - Bộ Đếm Tăng Giám - VueX
+**VueX** Chức Năng Dùng Để Quản Lý Trạng Thái (State Management Library) giống **Redux**
+## Công Nghệ Sử Dụng
+- Vue 3
+- VueX: `import { useCounterStore } from "@/stores/counter";` thư viện của VueX (chưa dùng Package chính thức của VueX)
+- Vue-Router
+- Pinia
+- Responsive Website và Mobile
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="main-section">
+    <header>
+      <h1>Counter App - Bộ Đếm Tăng Giám - <span>VueX</span></h1>
+    </header>
+
+    <p class="count">{{ counterStore.count }}</p>
+    <div class="counter">
+      <button class="btn-donate" @click="counterStore.decrement">
+        Giảm
+      </button>
+      <button @click="counterStore.reset">
+        <span class="shadow"></span>
+        <span class="edge"></span>
+        <span class="front text"> Reset </span>
+      </button> 
+      <button class="btn-donate" @click="counterStore.increment">
+        Tăng
+      </button>
+    </div> 
+  </div> 
+</template>
+  
+<script>
+import { useCounterStore } from "@/stores/counter";
+export default {
+  setup() {
+    const counterStore = useCounterStore();
+
+    return {
+      counterStore,
+    };
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.main-section {
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    position: absolute;
+    gap: 20px;
+    width: 100%;
+    top: 0;
+    margin-top: 250px;
+}
+
+.count {
+  font-size: 40px;
+  font-weight: bold;
+}
+
+h1{
+  color: white;
+  font-weight: bold;
+  
+}
+
+span{
+  color: #47ba87;
+  font-weight: bold;
+}
+
+p{
+  color: white;
+}
+
+.counter {
+  display: flex;
+  gap: 30px;
+}
+
+button {
+  position: relative;
+  border: none;
+  background: transparent;
+  padding: 0;
+  cursor: pointer;
+  outline-offset: 4px;
+  transition: filter 250ms;
+  user-select: none;
+  touch-action: manipulation;
+}
+
+.shadow {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  background: hsl(0deg 0% 0% / 0.25);
+  will-change: transform;
+  transform: translateY(2px);
+  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+}
+
+.edge {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 12px;
+  background: linear-gradient(
+    to left,
+    hsl(340deg 100% 16%) 0%,
+    hsl(340deg 100% 32%) 8%,
+    hsl(340deg 100% 32%) 92%,
+    hsl(340deg 100% 16%) 100%
+  );
+}
+
+.front {
+  display: block;
+  position: relative;
+  padding: 12px 27px;
+  border-radius: 12px;
+  font-size: 1.1rem;
+  color: white;
+  background: hsl(345deg 100% 47%);
+  will-change: transform;
+  transform: translateY(-4px);
+  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+}
+
+button:hover {
+  filter: brightness(110%);
+}
+
+button:hover .front {
+  transform: translateY(-6px);
+  transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+}
+
+button:active .front {
+  transform: translateY(-2px);
+  transition: transform 34ms;
+}
+
+button:hover .shadow {
+  transform: translateY(4px);
+  transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+}
+
+button:active .shadow {
+  transform: translateY(1px);
+  transition: transform 34ms;
+}
+
+button:focus:not(:focus-visible) {
+  outline: none;
+}
+
+.btn-donate {
+  --clr-font-main: hsla(0 0% 20% / 100);
+  --btn-bg-1: hsla(194 100% 69% / 1);
+  --btn-bg-2: hsla(217 100% 56% / 1);
+  --btn-bg-color: hsla(360 100% 100% / 1);
+  --radii: 0.5em;
+  cursor: pointer;
+  padding: 0.9em 1.4em;
+  min-width: 120px;
+  min-height: 44px;
+  font-size: var(--size, 1rem);
+  font-family: "Segoe UI", system-ui, sans-serif;
+  font-weight: 500;
+  transition: 0.8s;
+  background-size: 280% auto;
+  background-image: linear-gradient(
+    325deg,
+    var(--btn-bg-2) 0%,
+    var(--btn-bg-1) 55%,
+    var(--btn-bg-2) 90%
+  );
+  border: none;
+  border-radius: var(--radii);
+  color: var(--btn-bg-color);
+  box-shadow: 0px 0px 20px rgba(71, 184, 255, 0.5),
+    0px 5px 5px -1px rgba(58, 125, 233, 0.25),
+    inset 4px 4px 8px rgba(175, 230, 255, 0.5),
+    inset -4px -4px 8px rgba(19, 95, 216, 0.35);
+}
+
+.btn-donate:hover {
+  background-position: right top;
+}
+
+.btn-donate:is(:focus, :focus-visible, :active) {
+  outline: none;
+  box-shadow: 0 0 0 3px var(--btn-bg-color), 0 0 0 6px var(--btn-bg-2);
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .btn-donate {
+    transition: linear;
+  }
+}
+
+@media (max-width: 768px) {
+  h1{
+    font-size: 18px;
+  }
+  .counter {
+  display: flex;
+  gap: 15px;
+}
+}
+</style>
